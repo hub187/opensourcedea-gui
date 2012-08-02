61f7e42d9b2c80bfe16e8a7e91d11d5a001d4c24
hub <hubert.virtos@gmail.com>
2012-08-02 22:32:16 +0100

    Fixed a bug on the SolvingThread for when the lpsolve libraries are not installed correctly

--- OSDEA_GUI/src/org/opensourcedea/gui/maingui/SolvingThread.java
+++ OSDEA_GUI/src/org/opensourcedea/gui/maingui/SolvingThread.java
@@ -17,15 +17,17 @@ public class SolvingThread extends Thread {
 	private Navigation nav;
 	private OSDEA_StatusLine stl;
 	private volatile boolean stopRequested = false;
+	private volatile boolean errorStop = false;
+	private String errStr = "";
 	private LDEAProblem ldeap;
 	private DEAPProblemComposite comp;
 	private SolvingProgress progress;
-	
-	
-	
+
+
+
 	public SolvingThread(LDEAProblem ldeap, DEAProblem deap, Display display, Navigation nav, OSDEA_StatusLine stl, Button solveButton,
 			DEAPProblemComposite comp, SolvingProgress progress) {
-		
+
 		this.deap = deap;
 		this.display = display;
 		this.nav = nav;
@@ -34,9 +36,9 @@ public class SolvingThread extends Thread {
 		this.comp = comp;
 		this.progress = progress;
 	}
-	
-	
-	
+
+
+
 	public void run() {
 
 		try {
@@ -47,21 +49,31 @@ public class SolvingThread extends Thread {
 				if(stopRequested) {
 					Thread.currentThread().interrupt();
 					display.syncExec(new Runnable() {
-							public void run() {
-								stl.setNotificalLabelDelayStandard("Solving cancelled.");
-								comp.showProgressGroupNotSolved(OSDEAConstants.solveButtonText);
-							}});
+						public void run() {
+							stl.setNotificalLabelDelayStandard("Solving cancelled.");
+							comp.showProgressGroupNotSolved(OSDEAConstants.solveButtonText);
+						}});
+					return;
+				}
+				if(errorStop) {
+					Thread.currentThread().interrupt();
+					display.syncExec(new Runnable() {
+						public void run() {
+							MessageDialog.openError(nav.getShell(), "Error!", errStr);
+							stl.setNotificalLabelDelayStandard("Solving error!");
+							comp.showProgressGroupNotSolved(OSDEAConstants.solveButtonText);
+						}});
 					return;
 				}
 				solve(deap, nbDMUs, i);
 			}
-			
+
 			display.syncExec(new Runnable() {
 				public void run() {
 					comp.showProgressGroupSolved();
 				}
 			});
-			
+
 			copyAndDisplaySolution(deap);
 		}
 		catch (Exception ex) {
@@ -76,21 +88,21 @@ public class SolvingThread extends Thread {
 				}});
 		}
 	}
-	
-	
-	
+
+
+
 	public void sendStopRequest() {
-		
+
 		stopRequested = true;
 		if (this != null) {
 			Thread.currentThread().interrupt();
 		}
 	}
-	
-	
-	
+
+
+
 	private void copyAndDisplaySolution(DEAProblem deap) throws ProblemNotSolvedProperlyException, IncompatibleModelTypeException {
-		
+
 		ldeap.copyLDEAPSolution(deap);
 
 		display.syncExec(new Runnable() {
@@ -106,21 +118,35 @@ public class SolvingThread extends Thread {
 			}});
 
 	}
-	
+
 
 	private void solve(final DEAProblem deap, final int nbDMUs, int i) {
-
+		
 		final int pos = i;
+		final Integer dmu = pos + 1;
+		try {
+			deap.solveOne(pos);
+		} catch (Exception e) {
+			errorStop = true;
+			errStr = "There was an error while solving the problem. It is likely this is caused by some inconsistencies in the data.";
+			if (this != null) {
+				Thread.currentThread().interrupt();
+			}
+			return;
+		}
+		catch (Error e1) { //java.lang.NoClassDefFoundError || UnsatisfiedLinkError
+			errorStop = true;
+			errStr = "The lpsolve library could not be found. Please check the installation instructions and make sure the lpsolve libraries " +
+					"are correctly installed on your system and restart OSDEA.";
+			if (this != null) {
+				Thread.currentThread().interrupt();
+			}
+			return;
+		}
 		display.syncExec(new Runnable() {
 			public void run() {
-				Integer dmu = pos + 1;
-				try {
-					deap.solveOne(pos);
-					Integer perc = 100 * dmu / nbDMUs;
-					progress.updateProgressLabelText(OSDEAConstants.getSolvedDMUsProgress(dmu, (Integer)nbDMUs, perc));
-				} catch (Exception e) {
-					//
-				}
+				Integer perc = 100 * dmu / nbDMUs;
+				progress.updateProgressLabelText(OSDEAConstants.getSolvedDMUsProgress(dmu, (Integer)nbDMUs, perc));
 
 				progress.setProgressBar(((double)pos+1)/(double)nbDMUs);
 
