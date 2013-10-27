3932d2c3af48d07fd910e5ced5dbe371d4c3185e
hubert <hubert.virtos@gmail.com>
2013-10-27 12:21:53 +0100

    Cleared the gitignore file which ignored all new files created. Now can commit some fairly important changes (e.g. VariablesStatusenum etc.)!

---
+++ OSDEA_GUI/src/org/opensourcedea/gui/maingui/VariablesStatusEnum.java
@@ -0,0 +1,16 @@
+package org.opensourcedea.gui.maingui;
+
+/**
+ * Enum used to check the status of variables
+ * @author hubert (leonard)
+ *
+ */
+public enum VariablesStatusEnum {
+	
+	NoVariables,
+	allVariablesCorrectlySet,
+	NoInputSelected,
+	NoOutputSelected,
+	NotAllVariablesSelected;
+	
+}
